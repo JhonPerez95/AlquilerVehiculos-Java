d517919b1623133ff06bfa84114f9373f0897d5b
jhonperez95 <jhonperez.j95@gmail.com>
2022-07-19 11:58:43 -0500

    terminando el proyecto

--- src/alquiler/alquilerVehi.java
+++ src/alquiler/alquilerVehi.java
@@ -12,62 +12,90 @@
 
 package alquiler;
 
-public class alquilerVehi extends principal{
-  
+import javax.swing.JOptionPane;
+
+public class alquilerVehi extends principal {
+
     int coche = 0;
     int microbus = 0;
-    int carga =0;
-    int camion =0;
-    int numDias=0; // atributos que hererada el metodo
-    double result=0;
+    int carga = 0;
+    int camion = 0;
+    int numDias = 0; // atributos que hererada el metodo
+    double result = 0;
     String placa = "";
-    int tipoVehiculo=0; //atributos que hererada el metodo
-    int tipo=0;
-    
-    //exepciones verificadas que resultan de IOExeption
+    int tipoVehiculo = 0; // atributos que hererada el metodo
+    int tipo = 0;
+
+    // exepciones verificadas que resultan de IOExeption
     // no dependen del programador pero java aobliga a la solucion
-   //exepciones NO verificadas que resutan de runTimeExeption 
-   // (las soluciona el programados como una division de un entoreo en 0
-    
-    public alquilerVehi (int numDias,int tipoVehiculo){
+    // exepciones NO verificadas que resutan de runTimeExeption
+    // (las soluciona el programados como una division de un entoreo en 0
+
+    public alquilerVehi(int numDias, int tipoVehiculo, String placa) {
         this.numDias = numDias;
         this.tipoVehiculo = tipoVehiculo;
-    }
-    public alquilerVehi (String placa){ // polimorfirmos en la misma clase se denomina sebre carga de metodoso
         this.placa = placa;
     }
-   private void coche (){ //metodos o funciones  //encapsulamiento
-     result = numDias * (50 + 1.5); //modularidad
-     System.out.println ("valor: " + result + "   por : " + numDias + "  dias a alquilar"); 
+
+    public alquilerVehi(String placa) { // polimorfirmos en la misma clase se
+        // denomina sebre carga de metodoso
+        this.placa = placa;
     }
-    private void microbus (){ //metodos o funciones 
-         result = numDias * (50 + 1.5); 
-         System.out.println ("valor: " + result + "   por : " + numDias + "  dias a alquilar"); 
+
+    private void coche() { // metodos o funciones //encapsulamiento
+        result = numDias * (50 + 1.5); // modularidad
+
+        JOptionPane.showMessageDialog(null,
+                "Vehiculo tipo Automovil:\n"
+                        + "- Valor: " + result + "\n"
+                        + "- Dias Alquilados: " + numDias + "\n"
+                        + "- Placa Vehiculo: " + placa);
+
+        // System.out.println("valor: " + result + " por : " + numDias + " dias a
+        // alquilar");
     }
-     private void carga (){ //metodos o funciones 
-         result = numDias * (50 + 20); 
-         System.out.println ("valor: " + result + "   por : " + numDias + "  dias a alquilar" ); 
+
+    private void microbus() { // metodos o funciones
+        result = numDias * (50 + 1.5);
+
+        JOptionPane.showMessageDialog(null,
+                "Vehiculo tipo Microbus:\n"
+                        + "- Valor: " + result + "\n"
+                        + "- Dias Alquilados: " + numDias + "\n"
+                        + "- Placa Vehiculo: " + placa);
     }
-     private void camion(){//metodos o funciones 
-         result = numDias * 50; 
-         System.out.println ("valor: " + result + "   por : " + numDias + "  dias a alquilar"); 
-     }  
-     public void tipo (){ //metodos o funciones 
-         if(tipoVehiculo==1){
-           System.out.println ("Vehiculo tipo Automovil"); 
-          coche();//llamo una funcion
-         }else if (tipoVehiculo==2) {
-            System.out.println ("Vehiculo tipo Microbus");
-            microbus(); //llamo una funcion
-         } else if (tipoVehiculo==3) {
-            System.out.println ("Vehiculo tipo Furgon de carga");
-            carga();//llamo una funcion
-         }else if (tipoVehiculo==4) {
-            System.out.println ("Vehiculo tipo Camion");
-            camion(); //llamo una funcion
-        }else {
-            System.out.println ("Tipo de vehiculo no existe...");
-        }  
-     }             
+
+    private void carga() { // metodos o funciones
+        result = numDias * (50 + 20);
+        JOptionPane.showMessageDialog(null,
+                "Vehiculo tipo Furgon de carga:\n"
+                        + "- Valor: " + result + "\n"
+                        + "- Dias Alquilados: " + numDias + "\n"
+                        + "- Placa Vehiculo: " + placa);
+    }
+
+    private void camion() {// metodos o funciones
+        result = numDias * 50;
+        JOptionPane.showMessageDialog(null,
+                "Vehiculo tipo Camion:\n"
+                        + "- Valor: " + result + "\n"
+                        + "- Dias Alquilados: " + numDias + "\n"
+                        + "- Placa Vehiculo: " + placa);
+
+    }
+
+    public void tipo() { // metodos o funciones
+        if (tipoVehiculo == 1) {
+            coche();// llamo una funcion
+        } else if (tipoVehiculo == 2) {
+            microbus(); // llamo una funcion
+        } else if (tipoVehiculo == 3) {
+            carga();// llamo una funcion
+        } else if (tipoVehiculo == 4) {
+            camion(); // llamo una funcion
+        } else {
+            JOptionPane.showMessageDialog(null, "Vehiculo no existe!");
+            System.exit(0);
+        }
+    }
 }
-    
--- src/alquiler/principal.java
+++ src/alquiler/principal.java
@@ -6,31 +6,21 @@ import java.util.Scanner;
 
 public class principal {// herencia minimo deben existir dos clases
 
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        public static void main(String[] args) {
 
-        String placa = JOptionPane.showInputDialog(null,
-                "------------ALQUILER VEHICULO-----------\n"
-                        + "Ingresar la placa del vehiculo\n");
+                String placa = JOptionPane.showInputDialog(null,
+                                "------------ALQUILER VEHICULO-----------\n"
+                                                + "Ingresar la placa del vehiculo\n");
 
-        int numDias = Integer.parseInt(JOptionPane.showInputDialog(null, "------------ALQUILER VEHICULO-----------\n"
-                + "Ingrese dias a alquilar:"));
+                int numDias = Integer
+                                .parseInt(JOptionPane.showInputDialog(null, "------------ALQUILER VEHICULO-----------\n"
+                                                + "Ingrese dias a alquilar:"));
 
-        int tipoVehiculo = Integer.parseInt(JOptionPane.showInputDialog(null,
-                "------------ALQUILER VEHICULO-----------\n"
-                        + "Ingrese Tipo Vehiculo: \n 1. Automovil \n 2. Microbus \n 3. Furgon \n 4. Camion"));
+                int tipoVehiculo = Integer.parseInt(JOptionPane.showInputDialog(null,
+                                "------------ALQUILER VEHICULO-----------\n"
+                                                + "Ingrese Tipo Vehiculo: \n 1. Automovil \n 2. Microbus \n 3. Furgon \n 4. Camion"));
 
-        // System.out.println("Ingrese placa:");
-        // String placa = entrada.nextLine();
-        // System.out.println("Ingrese dias a alquilar:");
-        // int numDias = entrada.nextInt();
-        // System.out.println("Ingrese Tipo Vehiculo: \n 1. Automovil \n 2. Microbus \n
-        // 3. Furgon \n 4. Camion");
-        // int tipoVehiculo = entrada.nextInt();
-
-        alquilerVehi mensajero = new alquilerVehi(numDias, tipoVehiculo); // polimorfismo
-        mensajero.tipo();
-
-        System.out.println("Placa: " + placa);
-    }
+                alquilerVehi mensajero = new alquilerVehi(numDias, tipoVehiculo, placa); // polimorfismo
+                mensajero.tipo();
+        }
 }
